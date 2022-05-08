dc1547cbb30a3058dbf2a70605c27920104e0a20
Breno Angelo <brenoangeloal@gmail.com>
2022-05-07 23:00:44 -0300

    implement send mail and feedbacks endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,46 @@
-import express from 'express'
+import express from 'express';
+import nodemailer from 'nodemailer'
+import { prisma } from './prisma';
 
-const app = express()
+const app = express();
 
-app.get('/users', (req, res) => {
-  return res.send('Hello World');
-})
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "0689418bc944e8",
+    pass: "7e6a9c38402354"
+  }
+});
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Breno Angelo <brenoangeloal@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color #111">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
+  })
+
+  return res.status(201).json({ data: feedback })
+});
 
 app.listen(3333, () => {
-  console.log('HTTP server running!')
-})
+  console.log('HTTP server running!');
+});
